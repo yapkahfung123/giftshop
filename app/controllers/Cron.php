631f44213d89948bca4dae07c6a613f78cf84e91
yapkahfung <kahfungya@bravonet.my>
2020-05-25 11:54:11 +0800

    cron

--- app/controllers/Cron.php
+++ app/controllers/Cron.php
@@ -35,7 +35,7 @@ class Cron extends Controller
             $mail->isHTML(true);                                  // Set email format to HTML
             $mail->Subject = 'Here is the subject';
             $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->AltBody = 'This is the body in from production cron job';
 
             $mail->send();
             echo 'Message has been sent';
