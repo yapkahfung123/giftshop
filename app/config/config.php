ac980f7b8c8e22b862a8a38efe778b15b87de4a7
yapkahfung <kahfungya@bravonet.my>
2020-06-05 00:07:43 +0800

    config

--- app/config/config.php
+++ app/config/config.php
@@ -1,26 +1,23 @@
 <?php
 //DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'giftshop');
+define('DB_USER', 'ymcstore_giftshop');
+define('DB_PASS', 'qu41yN5#');
+define('DB_NAME', 'ymcstore_giftshop_db');
+
 
 //SHOW ERROR
-define('DEBUG', true);
+define('DEBUG', false);
 
 // APP ROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URL ROOT
-define('URLROOT', 'http://giftshop.test/');
+define('URLROOT', 'https://www.giftshop.ymcstore.com/');
 
 // Site Name
 define('SITENAME', 'YMCSTORE');
 
-// Define BasePath
-define('BASEPATH', __DIR__);
-
 //API TOKEN
 define('API_TOKEN', '9FsNBTD9!@$#YKFFS');
-
 
