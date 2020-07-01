9d50ce123a19ccab1fbf93e33a0e63272bfdcf11
yapkahfung123 <50409652+yapkahfung123@users.noreply.github.com>
2020-07-01 15:03:45 +0800

    redirect to https if not

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 require_once '../app/bootstrap.php';
 
+if($_SERVER['REQUEST_SCHEME'] != 'https'){
+    redirect('');
+}
+
 ini_set('display_errors', DEBUG);
 ini_set('display_startup_errors', DEBUG);
 error_reporting(E_ALL);
